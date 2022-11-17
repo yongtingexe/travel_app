55b38a883f3a58f92c05cd6508dec48e630cd1dc
yongtingexe <114375331+yongtingexe@users.noreply.github.com>
2022-11-17 15:12:56 +0800

    Add files via upload

---
+++ travel app/travel app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  travel app
+//
+//  Created by kdu on 17/11/2022.
+//  Copyright © 2022 uow. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ travel app/travel appTests/travel_appTests.swift
@@ -0,0 +1,34 @@
+//
+//  travel_appTests.swift
+//  travel appTests
+//
+//  Created by kdu on 17/11/2022.
+//  Copyright © 2022 uow. All rights reserved.
+//
+
+import XCTest
+@testable import travel_app
+
+class travel_appTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
